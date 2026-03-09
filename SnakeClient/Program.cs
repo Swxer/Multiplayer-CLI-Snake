70c8a9153309b343815c95e8a5f5b20103c50ca2
Swxer <swxergamer64@gmail.com>
2026-03-09 20:03:38 +1100

    Implement basic SignalR setup

--- SnakeClient/Program.cs
+++ SnakeClient/Program.cs
@@ -1,3 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using Microsoft.AspNetCore.SignalR.Client;
+
+public class Program
+{
+    public static async Task Main()
+    {
+        var connection = new HubConnectionBuilder();
+        connection.WithUrl("http://localhost:5000/gameHub");
+        var hubConnection = connection.Build();
+        await hubConnection.StartAsync();
+        hubConnection.On<string>("Snake Move", (message) => {
+            Console.WriteLine(message); // Handle incoming message
+        });
+        
+        await hubConnection.InvokeAsync("Move", "Down");
+        
+        Console.ReadLine();
+
+    }
+}
---
+++ SnakeServer/GameHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Collections.Concurrent;
+
+namespace SnakeServer;
+
+public class GameHub : Hub
+{
+    public static readonly ConcurrentDictionary<string, Snake> Players = new();
+
+    public override async Task OnConnectedAsync()
+    {
+        await Clients.All.SendAsync("Snake Oil", $"Player {Context.ConnectionId} connected");
+  
+    }
+
+    public async Task Move(string direction)
+    {
+        await Clients.All.SendAsync("Snake Move", $"Player {Context.ConnectionId} moving {direction}");
+    }
+}
