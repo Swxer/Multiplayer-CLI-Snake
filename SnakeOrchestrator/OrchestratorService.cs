1da2eff1bbb8b39549904447e07605e8dbff08f9
Swxer <swxergamer64@gmail.com>
2026-03-24 00:22:56 +1100

    Created OrchestratorService w/ basic server instance configurations

---
+++ SnakeOrchestrator/OrchestratorService.cs
@@ -0,0 +1,22 @@
+﻿namespace SnakeOrchestrator;
+using System.Net.Http.Json;
+using Docker.DotNet;
+using Docker.DotNet.Models;
+
+public class OrchestratorService
+{
+    private readonly DockerClient _docker;
+    private readonly List<ServerInstance> _servers = [];
+    
+    private const int MinServerPort = 8080;
+    private const int MaxServerPort = 8085;
+
+    private const int MaxPlayerPerServer = 3;
+    private const int EmptyTimeOutSeconds = 60;
+
+    public OrchestratorService()
+    {
+        _docker =  new DockerClientConfiguration().CreateClient();
+    }
+
+}
